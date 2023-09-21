c93142d7b8618dcc371b0f000f61caa034123bd6
traoredramane <trdramane98@gmail.com>
2023-09-21 14:21:03 +0400

    debut

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    
+    
+</head>
+<body>
+   
+<section class="header">
+        <nav>
+            <a href="index.html"><img src="logo.jpg" alt="" /></a>
+            <div class="nav-links" id="navLinks">
+                <i class="fa fa-solid fa-xmark" onclick="hideMenu()"></i>
+                <ul>
+                    <li><a href="index.html">ACCEUIL</a></li>
+                    <li><a href="activite.html">NOS ACTIVITES</a></li>
+                    <li><a href="">NOS DESTINATIONS</a></li>
+                    <li><a href="">NOUS CONTACTER</a></li>
+                </ul>
+            </div>
+            <i class="fa fa-solid fa-bars fa" onclick="showMenu()"></i>
+        </nav>
+
+        <div class="text-box">
+           <marquee behavior="" direction="right"><h1>Compagnie de Transport et Tourisme</h1></marquee> 
+           
+           <h2>Faites le tour du Burkina Faso</h2>
+           
+           <marquee behavior="" direction="left"> <p>Le Burkina Faso, un pays de diversité touristique avec de forte potentiel culturel nous invites a le visité.</p></marquee>
+           
+            <a href="" class="hero-btn">Contacter nous pour plus d'infos</a>
+        </div>
+    </section><br><br>
+    
+    
+    <section class="vil">
+    <h1>Trois grandes villes touristique du Burkina Faso</h1>
+    <p>Au Burkina Faso, l'ensemble de son milieu regorge de nombreux potentiel site touristique. Il renferme une diversité de site naturel qui est sacré et qui attire nombreuse personne à cela s'ajoute sa diversité culturelle qui offre plusieurs traditions à travers leur pratiquent ancienne dont la plupart attire de nombreux touristes à venir visiter. Ces differentes allegation nous amménes à la decouverte de trois(3) grandes villes touristiques du BF qui regorgent de nombreux site touristique dont certains sont dejà inscrit dans le patrimoine mondial de l'UNESCO. Pour en savoir plus, nous irons à leur rencontre.</p>
+   
+   <h2>Allons à la decouverte</h2>
+   
+    <div class="rows vile">
+   
+    <div class="col-vil">
+   <h2>Le centre ville</h2>
+    <img class="ma1" src="mairie.jpg" alt="">
+    </div>
+    
+    
+     
+    <div class="col-vil">
+    
+    <h2>La mosquée de Dioulassoba</h2>
+    <img src="mosque.jpg" alt="">
+    </div>
+    
+    
+    
+    <div class="col-vil">
+    
+    <h2>La carte du Burkina</h2>
+    <img src="carte.jpg" alt="">
+    </div>
+    
+    
+    
+    </div>
+    
+    
+    
+    </section><br><br>
+    
+    
+</body>
+</html>
+
+
+<?php include("footer.php") ?>
